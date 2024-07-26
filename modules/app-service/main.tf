e787e9314c9832ffbfa942d8f3339e3e8e885205
George Caws <george.caws@edgehealth.co.uk>
2024-07-26 11:32:17 +0100

    adding secret name

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -22,6 +22,7 @@ resource "time_rotating" "sp_dap_alpha_auth_rotation" {
 
 resource "azuread_service_principal_password" "sp_dap_alpha_auth_secret" {
   service_principal_id = azuread_service_principal.sp_dap_alpha_auth.object_id
+  display_name         = "${var.resource_prefix}-auth-secret-${var.environment}"
   rotate_when_changed = {
     rotation = time_rotating.sp_dap_alpha_auth_rotation.id
   }
