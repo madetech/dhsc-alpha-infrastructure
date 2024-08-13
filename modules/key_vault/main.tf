6807587a239d6540a349ddc2e8b88630459eccea
George Caws <george.caws@edgehealth.co.uk>
2024-08-13 13:58:54 +0100

    Groups for keyvault access

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -4,6 +4,17 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "tenant_id" {}
 
+resource "azuread_group" "secret_readers" {
+  display_name     = format("%s - %s", "DAP Alpha - Secret readers", upper(var.environment))
+  security_enabled = true
+}
+
+resource "azuread_group" "secret_readers" {
+  display_name     = format("%s - %s", "DAP Alpha - Key vault admin", upper(var.environment))
+  security_enabled = true
+}
+
+
 resource "azurerm_key_vault" "key_vault" {
   name                     = "${var.resource_prefix}-kv-${var.environment}"
   location                 = var.location
