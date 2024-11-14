806f47bfee486189ba62399c981885bad2c6dfda
Sam Goulden <sam.goulden@edgehealth.co.uk>
2024-11-14 16:34:34 +0000

    databricks workspace for ai

--- main.tf
+++ main.tf
@@ -254,7 +254,7 @@ module "datalake" {
 }
 
 module "databricks_workspace" {
-  source                   = "./modules/databricks_workspace"
+  source                   = "./modules/databricks_workspace_data"
   environment              = var.environment
   resource_prefix          = var.resource_prefix
   resource_group_name      = azurerm_resource_group.rg_data.name
@@ -263,7 +263,7 @@ module "databricks_workspace" {
 }
 
 module "databricks_cluster" {
-  source                      = "./modules/databricks"
+  source                      = "./modules/databricks_data"
   environment                 = var.environment
   resource_prefix             = var.resource_prefix
   resource_group_name         = azurerm_resource_group.rg_data.name
@@ -282,6 +282,34 @@ module "databricks_cluster" {
   gold_primary_access_key     = module.datalake.gold_primary_access_key
 }
 
+module "databricks_workspace_ai" {
+  source                   = "./modules/databricks_workspace_ai"
+  environment              = var.environment
+  resource_prefix          = var.resource_prefix
+  resource_group_name      = azurerm_resource_group.rg_data.name
+  location                 = azurerm_resource_group.rg_data.location
+  data_factory_identity_id = azurerm_data_factory.adf_data.identity[0].principal_id
+}
+
+module "databricks_cluster_ai" {
+  source                      = "./modules/databricks_ai"
+  environment                 = var.environment
+  resource_prefix             = var.resource_prefix
+  resource_group_name         = azurerm_resource_group.rg_data.name
+  workspace_url               = module.databricks_workspace_ai.workspace_url
+  azure_msi_flag              = var.azure_msi_flag
+  workspace_id                = module.databricks_workspace_ai.workspace_id
+  storage_account_name        = azurerm_storage_account.sc_datalake.name # Alpha lake
+  string_value                = azurerm_storage_account.sc_datalake.primary_access_key # Alpha lake
+  drop_storage_account_name   = module.datalake.drop_storage_account_name
+  drop_primary_access_key     = module.datalake.drop_primary_access_key
+  bronze_storage_account_name = module.datalake.bronze_storage_account_name
+  bronze_primary_access_key   = module.datalake.bronze_primary_access_key
+  silver_storage_account_name = module.datalake.silver_storage_account_name
+  silver_primary_access_key   = module.datalake.silver_primary_access_key
+  gold_storage_account_name   = module.datalake.gold_storage_account_name
+  gold_primary_access_key     = module.datalake.gold_primary_access_key
+}
 
 # OpenAI resources
 module "openai" { 
